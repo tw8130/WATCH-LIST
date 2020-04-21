793d0c1f7e5afec2a82832fb440655f97dacccda
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-04-21 19:05:10 +0300

    create movie.html file to use the dynamic routes concept

--- app/views.py
+++ app/views.py
@@ -2,10 +2,10 @@ from flask import render_template
 from app import app
 
 # Views
-@app.route('/')
-def index():
+@app.route('/movie/<int:movie_id>')
+def movie(movie_id):
 
     '''
-    View root page function that returns the index page and its data
+    View movie page function that returns the movie details page and its data
     '''
-    return render_template('index.html')
+    return render_template('movie.html',id = movie_id)
