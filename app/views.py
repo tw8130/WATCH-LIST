0d2c4a042e14090711fb7f3959d3731d814870fc
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-04-21 21:25:17 +0300

    update our files in init.py and config.py for the creating configuration classes and update views.py and index.html for the control flows in jinja template concept

--- app/views.py
+++ app/views.py
@@ -2,6 +2,17 @@ from flask import render_template
 from app import app
 
 # Views
+@app.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title = title)
+
+
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
 
