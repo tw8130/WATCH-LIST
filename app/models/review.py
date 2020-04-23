1f9bbecfbbdd8b5779ebe913d2e82aa2747e2f95
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-04-23 13:43:40 +0300

    add docstrings

--- app/models/review.py
+++ app/models/review.py
@@ -1,8 +1,15 @@
 class Review:
-
+    '''
+    Class that generates new instances of reviews
+    '''
+    #class variable
     all_reviews = []
 
     def __init__(self,movie_id,title,imageurl,review):
+        '''
+        helps us define properties for review objects
+        '''
+
         self.movie_id = movie_id
         self.title = title
         self.imageurl = imageurl
@@ -10,9 +17,15 @@ class Review:
 
 
     def save_review(self):
+        '''
+        appends review object to the class variable
+        '''
         Review.all_reviews.append(self)
 
 
     @classmethod
     def clear_reviews(cls):
+        '''
+        clears all the items from list
+        '''
         Review.all_reviews.clear()
