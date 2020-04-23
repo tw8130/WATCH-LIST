74e07c10246c3dfdf5761b09667c9f3293e97d4e
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-04-23 15:19:48 +0300

    add doctrings to explain the test cases

--- app/review_test.py
+++ app/review_test.py
@@ -3,8 +3,15 @@ from models import review
 Review = review.Review
 
 class TestReview(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Review class
+    '''
+
 
     def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
         self.new_review = Review(12345,'Review for movies',"https://image.tmdb.org/t/p/w500/jdjdjdjn",'This movie is the best thing since sliced bread')
 
 
@@ -12,10 +19,16 @@ class TestReview(unittest.TestCase):
         Review.clear_reviews()
 
     def test_instance(self):
+        '''
+        test case that uses the isinstance() to check if object is an instance of review class
+        '''
         self.assertTrue(isinstance(self.new_review,Review))
 
 
     def test_check_instance_variables(self):
+        '''
+        test that uses self.assertEqual to check for the expected  review results
+        '''
         self.assertEquals(self.new_review.movie_id,12345)
         self.assertEquals(self.new_review.title,'Review for movies')
         self.assertEquals(self.new_review.imageurl,"https://image.tmdb.org/t/p/w500/jdjdjdjn")
@@ -23,6 +36,9 @@ class TestReview(unittest.TestCase):
 
 
     def test_save_review(self):
+        '''
+        test that calls save_review() method to save our review objects
+        '''
         self.new_review.save_review()
         self.assertTrue(len(Review.all_reviews)>0)
 
