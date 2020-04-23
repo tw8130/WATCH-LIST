53bed2255bfe7ac1ec8563b1cdfb3a8661261a9b
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-04-23 17:04:05 +0300

    import review class from models folder and reviewform class from forms file and create dynamic route for new_review function and pass movie id for wft form concept

--- app/views.py
+++ app/views.py
@@ -1,7 +1,10 @@
 from flask import render_template,request,redirect,url_for
 from app import app
 from .request import get_movies,get_movie,search_movie
-# from .request import get_movies,get_movie
+from .models import review
+from .forms import ReviewForm
+Review = review.Review
+
 
 # Views
 @app.route('/')
@@ -18,7 +21,7 @@ def index():
     now_showing_movie = get_movies('now_playing')
     title = 'Home - Welcome to The best Movie Review Website Online'
 
-     search_movie = request.args.get('movie_query')
+    search_movie = request.args.get('movie_query')
 
     if search_movie:
         return redirect(url_for('search',movie_name=search_movie))
@@ -47,4 +50,18 @@ def search(movie_name):
     searched_movies = search_movie(movie_name_format)
     title = f'search results for {movie_name}'
     return render_template('search.html',movies = searched_movies)
-    
+
+@app.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+def new_review(id):
+    form = ReviewForm()
+    movie = get_movie(id)
+
+    if form.validate_on_submit():
+        title = form.title.data
+        review = form.review.data
+        new_review = Review(movie.id,title,movie.poster,review)
+        new_review.save_review()
+        return redirect(url_for('movie',id = movie.id ))
+
+    title = f'{movie.title} review'
+    return render_template('new_review.html',title = title, review_form=form, movie=movie)
