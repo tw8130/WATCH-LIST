a5b55da6b6e7529ac25d28816d91792228014a4d
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-04-24 17:21:16 +0300

    we are definig error handler in the blueprint so import blueprint instance main to define our decorator

--- app/main/error.py
+++ app/main/error.py
@@ -1,9 +1,34 @@
 from flask import render_template
-from app import app
+from . import main
 
-@app.errorhandler(404)
+@main.app_errorhandler(404)
 def four_Ow_four(error):
     '''
     Function to render the 404 error page
     '''
     return render_template('fourOwfour.html'),404
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from app import app
+
+# @app.errorhandler(404)
+# def four_Ow_four(error):
+#     '''
+#     Function to render the 404 error page
+#     '''
+#     return render_template('fourOwfour.html'),404
