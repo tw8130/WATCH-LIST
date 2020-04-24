25a2a019a45b0c77780b03741c2ab72ed7f516dc
Tabitha Wanjiku Mwangi <mwangitabitha2020@gmail.com>
2020-04-24 17:00:17 +0300

    update the file to create our application factory function

--- app/__init__.py
+++ app/__init__.py
@@ -2,21 +2,46 @@ from flask import Flask
 from .config import DevConfig 
 from flask_bootstrap import Bootstrap
 from flask_wtf.csrf import CSRFProtect
+from config import config_options
+
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Will add the views and forms
+
+    return app
 
 
-# Initializing application
-app = Flask(__name__,instance_relative_config = True)
 
 
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
-app.config['SECRET_KEY'] = "secretkey"
-app.config['WTF_CSRF_SECRET_KEY'] = "secretkey"
-csrf = CSRFProtect(app)
 
-# Initializing Flask Extensions
-bootstrap = Bootstrap(app)
 
-from app import views
-from app import error
+
+
+
+
+# # Initializing application
+# app = Flask(__name__,instance_relative_config = True)
+
+
+# # Setting up configuration
+# app.config.from_object(DevConfig)
+# app.config.from_pyfile('config.py')
+# app.config['SECRET_KEY'] = "secretkey"
+# app.config['WTF_CSRF_SECRET_KEY'] = "secretkey"
+# csrf = CSRFProtect(app)
+
+# # Initializing Flask Extensions
+# bootstrap = Bootstrap(app)
+
+# from app import views
+# from app import error
